691c8b87eaee81c902780c81de02340d87f3905e
MGX <2576285906@qq.com>
2020-08-08 14:18:00 +0800

    Site updated: 2020-08-08 14:18:00

--- js/godx.js
+++ js/godx.js
@@ -4,7 +4,7 @@ var top_up = "<img id='upj' class='upj' style='max-width: 250%; transform: trans
 document.getElementById("go-up").innerHTML += top_up;
 
 // 气泡
-/*function qipao() {
+function qipao() {
     $('#page-header').circleMagic({
         radius: 10,
         density: .2,
@@ -49,7 +49,7 @@ document.getElementById("go-up").innerHTML += top_up;
 }(jQuery);
 
 // 调用气泡方法
-qipao();*/
+qipao();
 // 可爱的Title
 var OriginTitle = document.title;
 var titleTime;
@@ -74,4 +74,27 @@ $(document).ready(function(e) {
 var full_page = document.getElementsByClassName("full_page");
 if (full_page.length != 0) {
   full_page[0].style.background = "transparent";
+}
+//分类卡片折叠
+var card_category_list = document.getElementsByClassName(
+    "card-category-list child"
+);
+var item = document.getElementsByClassName("card-category-list-item");
+function toggle(t) {
+    var display = t.parentElement.nextSibling.style.display;
+    if (display == "none") {
+        t.parentElement.nextSibling.style.display = "block";
+        t.parentElement.nextSibling.style.height = "100%";
+        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
+    } else {
+        t.parentElement.nextSibling.style.display = "none";
+        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
+    }
+}
+
+for (var i = 0; i < card_category_list.length; i++) {
+    card_category_list[i].style.display = "none";
+    card_category_list[i].style.transition = "all 1s";
+    card_category_list[i].previousSibling.innerHTML +=
+        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
 }
